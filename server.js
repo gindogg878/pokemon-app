1400b19e62bd9ab791baddd5e6dd79b3b0f51dbe
Gino Torres Jr <gindogg878@icloud.com>
2023-04-28 15:53:19 -0500

    connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const pokemon = require("./models/pokemon");
 
 //INDUCES for pokemon
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
 });
 
 //Listen
